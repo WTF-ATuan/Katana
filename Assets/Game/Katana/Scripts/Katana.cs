f0966163eae991c9759bde69c2bfcd9ae7e22229
ATuan <4A8K0069@stust.edu.tw>
2022-03-12 17:02:58 +0800

    fixed normal it,s still not perfect

--- Assets/Game/Katana/Scripts/Katana.cs
+++ Assets/Game/Katana/Scripts/Katana.cs
@@ -2,32 +2,19 @@ using System;
 using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Katana.Scripts{
 	public class Katana : MonoBehaviour{
-		private Transform _katanaModel;
+		[SerializeField] private Transform bladePoint;
 
-		[ShowInInspector]
-		[ReadOnly]
-		public Vector3 BladeDirection =>
-				_katanaModel.rotation * Vector3.up;
-
-		public Vector3 OriginPosition{
-			get{
-				var localShifted = _katanaModel.InverseTransformPoint(_katanaModel.position) + Vector3.down;
-				return _katanaModel.TransformPoint(localShifted);
-			}
-		}
-
-		[ShowInInspector]
-		[ReadOnly]
-		public Vector3 MoveDirection => _katanaModel.transform.position.normalized;
+		public Vector3 BladeDirection => bladePoint.forward;
+		public Vector3 BladePosition => bladePoint.position;
 
 		private Animator _animator;
 
 		private void Start(){
 			_animator = GetComponentInChildren<Animator>();
-			_katanaModel = GetComponentsInChildren<Transform>()[1];
 		}
 
 		public void PlayAnimation(Vector3 direction){
--- Assets/Game/Katana/Scripts/KatanaCollider.cs
+++ Assets/Game/Katana/Scripts/KatanaCollider.cs
@@ -14,29 +14,21 @@ namespace Katana.Scripts{
 			var enemy = other.GetComponent<Enemy.Scripts.Enemy>();
 			var sliceable = other.GetComponent<IBzSliceableNoRepeat>();
 			if(sliceable != null)
-				Slice(sliceable, other.transform.position);
+				Slice(sliceable, other);
 		}
 
-		private Vector3 normal;
 		private Vector3 collisionPoint;
 
-		private void Slice(IBzSliceableNoRepeat sliceableObject, Vector3 objectPosition){
-			normal = Vector3.Cross(_katana.MoveDirection, _katana.BladeDirection);
-			collisionPoint = GetCollisionPoint(objectPosition);
+		private void Slice(IBzSliceableNoRepeat sliceableObject, Collider other){
+			var normal = Vector3.Cross(_katana.BladePosition, _katana.BladeDirection);
+			Debug.Log($"normal = {normal}");
+			collisionPoint = other.ClosestPointOnBounds(_katana.BladePosition);
 			var plane = new Plane(normal, collisionPoint);
 			sliceableObject.Slice(plane, 0, result => Debug.Log(result.sliced));
 		}
 
-		private Vector3 GetCollisionPoint(Vector3 targetPosition){
-			var distanceToObject = targetPosition - _katana.OriginPosition;
-			var projectDot = Vector3.Project(distanceToObject, _katana.BladeDirection);
-			var collisionPoint = _katana.OriginPosition + projectDot;
-			return collisionPoint;
-		}
-
 		private void OnDrawGizmos(){
 			Gizmos.color = Color.green;
-			Gizmos.DrawWireSphere(normal, 0.1f);
 			Gizmos.DrawSphere(collisionPoint, 0.1f);
 		}
 	}
