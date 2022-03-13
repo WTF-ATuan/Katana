1c841be4a5da020dda09671266ae096a542d693a
ATuan <4A8K0069@stust.edu.tw>
2022-03-13 16:30:01 +0800

    remove slice vertex and slice function

--- Assets/Game/Katana/Scripts/Slicer.cs
+++ Assets/Game/Katana/Scripts/Slicer.cs
@@ -7,22 +7,6 @@ namespace Katana.Scripts{
 		public Vector3 PlaneNormal{ get; private set; }
 		public float PlaneDistance{ get; private set; }
 
-		public void Slice(Collider sliceCollider, SliceVertex vertex){
-			var sliceableObject = sliceCollider.GetComponent<IBzSliceableNoRepeat>();
-			if(sliceableObject == null) return;
-			var crossDirection = vertex.CrossDirection;
-			var collisionPosition = vertex.CollisionPosition;
-			var plane = CreatePlane(crossDirection, collisionPosition);
-			sliceableObject.Slice(plane, 0, OnSliced);
-		}
-
-		private void OnSliced(BzSliceTryResult obj){ }
-
-		public Plane CreatePlane(Vector3 inNormal, Vector3 inPoint){
-			var plane = new Plane(inNormal, inPoint);
-			PlaneNormal = plane.normal;
-			PlaneDistance = plane.distance;
-			return plane;
-		}
+		public void Slice(Collider sliceCollider){ }
 	}
 }
