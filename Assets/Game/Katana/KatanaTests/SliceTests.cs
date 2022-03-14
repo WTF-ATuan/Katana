5afd88362b3cfe615f19eabdd624665100d2032b
ATuan <4A8K0069@stust.edu.tw>
2022-03-15 00:02:53 +0800

    added test folder of katana

---
+++ Assets/Game/Katana/KatanaTests/SliceTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Game.Katana.KatanaTests{
+	public class SliceTests{
+		[Test]
+		public void Vector3CrossMethod(){
+			var point1 = Vector3.right;
+			var point2 = Vector3.forward;
+			var expectPoint = Vector3.up;
+			var crossPoint = Vector3.Cross(point1 , point2);
+			Assert.AreEqual(expectPoint , crossPoint);
+		}
+	}
+}
