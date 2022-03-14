19ddfe9a17e0576e3c505749edbff169e68a3650
ATuan <4A8K0069@stust.edu.tw>
2022-03-14 23:30:57 +0800

    removed unit test of Plug-in

---
+++ Assets/Game/Katana/Scripts/Slice/SliceableCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BzKovSoft.ObjectSlicer;
+using Game.Katana.Scripts.Slice.Interface;
+using Game.Project;
+using UnityEngine;
+
+namespace Game.Katana.Scripts.Slice{
+	public class SliceableCharacter : ISliceableNoRepeat{
+		private ColdDownTimer _timer;
+		
+		
+		
+		public void TrySlice(Plane plane, Action<BzSliceTryResult> callBack){
+			throw new NotImplementedException();
+		}
+	}
+}
