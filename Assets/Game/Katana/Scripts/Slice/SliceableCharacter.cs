5cc4b77dc89d73de494f6ebb6fc0ba022610e4e7
ATuan <4A8K0069@stust.edu.tw>
2022-03-14 23:54:22 +0800

    feat : sliceable Character worked

--- Assets/Game/Katana/Scripts/Slice/SliceableCharacter.cs
+++ Assets/Game/Katana/Scripts/Slice/SliceableCharacter.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using BzKovSoft.CharacterSlicer;
 using BzKovSoft.ObjectSlicer;
 using Game.Katana.Scripts.Slice.Interface;
 using Game.Project;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Game.Katana.Scripts.Slice{
-	public class SliceableCharacter : ISliceableNoRepeat{
-		private ColdDownTimer _timer;
-		
-		
-		
+	public class SliceableCharacter : BzSliceableCharacterBase, ISliceableNoRepeat{
+		[BoxGroup] [SerializeField] [Range(0.5f, 1.5f)]
+		private float sliceDelay = 1.0f;
+
+		private ColdDownTimer timer;
+
+		private void Start(){
+			timer = new ColdDownTimer(sliceDelay);
+		}
+
 		public void TrySlice(Plane plane, Action<BzSliceTryResult> callBack){
-			throw new NotImplementedException();
+			if(!timer.CanInvoke()) return;
+			Slice(plane, callBack);
+			timer.Reset();
 		}
 	}
 }
