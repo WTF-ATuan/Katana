6175f56ae8056863758de41def6357c6935cb3da
ATuan <4A8K0069@stust.edu.tw>
2022-03-22 12:33:55 +0800

    rewrite Enemy

--- Assets/Game/Enemy/Scripts/Enemy.cs
+++ Assets/Game/Enemy/Scripts/Enemy.cs
@@ -1,46 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace Enemy.Scripts{
 	public class Enemy : MonoBehaviour{
-		[SerializeField] private float moveSpeed = 0.05f;
-		public Vector3 TargetPosition{ get; private set; }
+		public Transform TargetTransform{ get; private set; }
 
-		public void SetTargetPosition(Vector3 position){
-			TargetPosition = position;
-		}
+		public float MainRoadDistance{ get; private set; }
+		public float MiddlePointDistance{ get; private set; }
 
-		public void Move(){
+		public float TargetDistance{ get; private set; }
+
+		public void ConditionDistance(){
+			if(!TargetTransform) throw new NullReferenceException("Target is Null");
 			var position = transform.position;
-			var moveDirection = (TargetPosition - position).normalized;
-			transform.position += moveDirection * moveSpeed;
-		}
-
-		public void SetFaceDirection(){
-			var position = transform.position;
-			var moveDirection = (TargetPosition - position).normalized;
-			transform.forward = moveDirection;
-		}
-
-		public float DistanceOfTarget(Vector3 target){
-			var position = transform.position;
-			var distance = Vector3.Distance(position, target);
-			return distance;
-		}
-
-
-		private Vector3 _hitFirstPoint, _hitLastPoint;
-		public void TakeDamage(List<Vector3> damagePoints){
-			_hitFirstPoint= damagePoints.First();
-			_hitLastPoint = damagePoints.Last();
-		}
-
-		private void OnDrawGizmos(){
-			Gizmos.color = Color.green;
-			Gizmos.DrawSphere(_hitFirstPoint , 0.1f);
-			Gizmos.DrawSphere(_hitLastPoint , 0.1f);
+			var targetPosition = TargetTransform.position;
+			var normal = targetPosition - position;
+			TargetDistance = Vector3.Distance(targetPosition, position);
+			MainRoadDistance = normal.x;
+			MiddlePointDistance = normal.z;
 		}
 	}
 }
