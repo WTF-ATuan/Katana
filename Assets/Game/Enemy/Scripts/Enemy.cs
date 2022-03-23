f2383a59af7afdc562e98e638181a6945f066ec5
ATuan <4A8K0069@stust.edu.tw>
2022-03-23 17:54:38 +0800

    enemy auto move

--- Assets/Game/Enemy/Scripts/Enemy.cs
+++ Assets/Game/Enemy/Scripts/Enemy.cs
@@ -1,23 +1,14 @@
 ﻿using System;
+using PlasticPipe.PlasticProtocol.Client;
 using UnityEngine;
 
 namespace Enemy.Scripts{
 	public class Enemy : MonoBehaviour{
 		public Transform TargetTransform{ get; private set; }
-
-		public float MainRoadDistance{ get; private set; }
-		public float MiddlePointDistance{ get; private set; }
-
-		public float TargetDistance{ get; private set; }
-
-		public void ConditionDistance(){
-			if(!TargetTransform) throw new NullReferenceException("Target is Null");
-			var position = transform.position;
-			var targetPosition = TargetTransform.position;
-			var normal = targetPosition - position;
-			TargetDistance = Vector3.Distance(targetPosition, position);
-			MainRoadDistance = normal.x;
-			MiddlePointDistance = normal.z;
+		
+		public void Initialize(Transform targetPoint){
+			TargetTransform = targetPoint;
 		}
+		
 	}
 }
