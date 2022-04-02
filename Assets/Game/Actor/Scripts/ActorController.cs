01b5e18ef81df4731239c822ca15606fa0bc254d
ATuan <4A8K0069@stust.edu.tw>
2022-04-02 16:29:01 +0800

    added SerializeEventHandler.cs want to serialize Event

--- Assets/Game/Actor/Scripts/ActorController.cs
+++ Assets/Game/Actor/Scripts/ActorController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Project;
 using Project.Gesture.Event;
 using UnityEngine;
---
+++ Assets/Game/Project/SerializeEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Game.Project{
+	//TODO 可以再編輯器當中設定要接收什麼Event，並且可以調整觸發內容;
+	public class SerializeEventHandler : MonoBehaviour{
+		
+	}
+}
