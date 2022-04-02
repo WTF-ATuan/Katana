9739c12e6c25dc8d741a05b8fc59964c7fce717b
ATuan <4A8K0069@stust.edu.tw>
2022-04-02 14:47:13 +0800

    added todo to actor and remove un-use function

--- Assets/Game/Actor/Scripts/Actor.cs
+++ Assets/Game/Actor/Scripts/Actor.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Actor.Scripts{
 	public class Actor : MonoBehaviour{
-
-		public void ModifyFaceDirectionAngle(float increasedAngle){
-			var checkValue = increasedAngle / 90;
-			var isInt = Mathf.Ceil(checkValue) == Mathf.Floor(checkValue);
-			if(!isInt) throw new Exception($"{increasedAngle}value is not right increasedAngle");
-			var actorTransform = transform;
-			var currentDirection = actorTransform.eulerAngles;
-			var nextDirection = new Vector3(currentDirection.x, currentDirection.y + increasedAngle, currentDirection.z);
-			actorTransform.eulerAngles = nextDirection;
-		}
-		public float GetCurrentFaceDirectionAngle(){
-			var eulerAngles = transform.eulerAngles;
-			return eulerAngles.y;
-		}
+		public Vector3 Position => transform.position;
 		
+		
+		
+		
+		//TODO when Katana Animation is Done we use EventBus to Handle 
 		public void Cleave(Katana.Scripts.Katana katana, Vector3 cleaveDirection){
 			katana.PlayAnimation(cleaveDirection);
 		}
