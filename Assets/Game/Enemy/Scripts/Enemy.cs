550f3eb3d4ba9989f8e4e4ca87af478e40134720
ATuan <4A8K0069@stust.edu.tw>
2022-04-12 21:37:52 +0800

    refactor enemy and AutoMoveBehavior.cs and added auto face Direction feature

--- Assets/Game/Enemy/Scripts/Enemy.cs
+++ Assets/Game/Enemy/Scripts/Enemy.cs
@@ -2,15 +2,24 @@
 
 namespace Enemy.Scripts{
 	public class Enemy : MonoBehaviour{
-		public Transform TargetTransform{ get; private set; }
-		
+		private Transform Target{ get; set; }
+
+		public Vector3 MiddlePosition =>
+				new(Target.position.x, transform.position.y, transform.position.z);
+
+		public Vector3 TargetPosition =>
+				new(Target.position.x, Target.position.y, Target.position.z + 2f);
+
+		public Vector3 Position => transform.position;
+
 		public void Initialize(Transform targetPoint){
-			TargetTransform = targetPoint;
+			Target = targetPoint;
 		}
 
-		public void AutoMove(float moveTime){
-			
+		public void SetFaceDirection(Vector3 targetPosition){
+			var enemyTransform = transform;
+			var targetDirection = (targetPosition - enemyTransform.position).normalized;
+			enemyTransform.forward = targetDirection;
 		}
-
 	}
 }
---
+++ Assets/Game/Enemy/Scripts/UseCase/AutoMoveBehavior.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Game.Enemy.Scripts.UseCase{
+	public class AutoMoveBehavior : MonoBehaviour{
+		[SerializeField] private Transform target;
+
+		private global::Enemy.Scripts.Enemy enemy;
+
+		private void Start(){
+			enemy = GetComponent<global::Enemy.Scripts.Enemy>();
+			enemy.Initialize(target);
+			AutoMove();
+		}
+
+		private void AutoMove(){
+			var middlePosition = enemy.MiddlePosition;
+			var targetPosition = enemy.TargetPosition;
+			enemy.SetFaceDirection(middlePosition);
+			var moveToMiddle = enemy.transform.DOMove(middlePosition, 3f);
+			moveToMiddle.OnComplete(() => {
+				enemy.transform.DOMove(targetPosition, 3f);
+				enemy.SetFaceDirection(targetPosition);
+			});
+		}
+	}
+}
