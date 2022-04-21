363b5c1ae83aec5e2d732a7edec5f0951e624449
ATuan <4A8K0069@stust.edu.tw>
2022-04-21 22:09:19 +0800

    fixed gesture data

--- Assets/Game/Actor/Scripts/GestureCalculator.cs
+++ Assets/Game/Actor/Scripts/GestureCalculator.cs
@@ -7,9 +7,9 @@ namespace Actor.Scripts{
 			var angle = Mathf.Atan2(lineDirection.x, lineDirection.y) * Mathf.Rad2Deg;
 			var finalAngle = angle > 0 ? angle : 180 + (180 + angle);
 			return finalAngle switch{
-				>= 330 or <= 30 => Vector3.up,
+				>= 330 or <= 30 => Vector3.down,
 				>= 40 and <= 140 => Vector3.right,
-				>= 150 and <= 210 => Vector3.down,
+				>= 150 and <= 210 => Vector3.up,
 				>= 220 and <= 320 => Vector3.left,
 				_ => Vector3.zero
 			};
