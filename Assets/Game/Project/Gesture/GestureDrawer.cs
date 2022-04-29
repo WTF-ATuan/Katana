ebfcb6b1e66c07e91420ac88e021af7bfb7fb034
Atuan <jain60501@gmail.com.tw>
2022-04-29 09:36:40 +0800

    Auto stash before merge of "master" and "origin/master"

--- Assets/Game/Project/Gesture/GestureDrawer.cs
+++ Assets/Game/Project/Gesture/GestureDrawer.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Project.Gesture.Event;
 using UnityEngine;
 
@@ -16,7 +15,7 @@ namespace Project.Gesture{
 		private void Start(){
 			_lineRenderer = GetComponent<LineRenderer>();
 			_pointList = new List<Vector3>();
-			_recognizer = new GestureRecognizer(Application.persistentDataPath);
+			_recognizer = new GestureRecognizer("Assets/Game/Project/Gesture/CustomData/");
 			_userCamera = !Camera.main ? FindObjectOfType<Camera>() : Camera.main;
 		}
 
