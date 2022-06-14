3247007eff3d43741ee6b49ae80da872cb40c172
Atuan <jain60501@gmail.com.tw>
2022-06-14 20:04:59 +0800

    added start Screen UI

---
+++ Assets/Game/UI/UIUtility.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI{
+	public class UIUtility : MonoBehaviour{
+		[SerializeField] private float during = 1.5f;
+
+
+		public void DoFadeOut(Image image){
+			image.DOFade(1, 0f);
+			image.DOFade(0, during)
+					.OnComplete(() => image.gameObject.SetActive(false));
+		}
+
+		public void DoFadeIn(Image image){
+			image.gameObject.SetActive(true);
+			image.DOFade(0, 0f);
+			image.DOFade(1, during);
+		}
+	}
+}
